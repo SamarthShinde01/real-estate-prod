3fb67ea17beba55471fa9e3cbeaf4789782bf12c
Samarth Shinde <samarthsshinde@gmail.com>
2025-02-27 11:43:24 +0530

    [client] updateTenantSettings & updateManagerSettings api AND settings page in managers & tenants

--- client/src/app/(dashboard)/tenants/settings/page.tsx
+++ client/src/app/(dashboard)/tenants/settings/page.tsx
@@ -1,5 +1,38 @@
+"use client";
+
+import SettingsForm from "@/components/SettingsForm";
+import {
+	useGetAuthUserQuery,
+	useUpdateTenantSettingsMutation,
+} from "@/state/api";
+import React from "react";
+
 const TenantSettings = () => {
-	return <div>TenantSettings</div>;
+	const { data: authUser, isLoading } = useGetAuthUserQuery();
+	const [updateTenant] = useUpdateTenantSettingsMutation();
+
+	if (isLoading) return <>Loading...</>;
+
+	const initialData = {
+		name: authUser?.userInfo.name,
+		email: authUser?.userInfo.email,
+		phoneNumber: authUser?.userInfo.phoneNumber,
+	};
+
+	const handleSubmit = async (data: typeof initialData) => {
+		await updateTenant({
+			cognitoId: authUser?.cognitoInfo?.userId,
+			...data,
+		});
+	};
+
+	return (
+		<SettingsForm
+			initialData={initialData}
+			onSubmit={handleSubmit}
+			userType="tenant"
+		/>
+	);
 };
 
 export default TenantSettings;
